9575f3f1dc1652412eeb84e929998cb70cef1cf3
Felipe Leal <Felileal25@gmail.com>
2026-04-05 00:06:14 -0300

    okis

--- api/log.js
+++ api/log.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
@@ -24,4 +24,4 @@ export default async function handler(req, res) {
   }
 
   res.status(200).json({ ok: true });
-}
+};
